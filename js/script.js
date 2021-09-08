a6296e001d1c9925043e58d3bd69a1559fa7ae0c
David Alex <DavidAlexx17@icloud.com>
2021-09-08 08:39:07 -0500

    attempts

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 const div = document.querySelector('div');
 
+const buttonOne = document.querySelector('#link-img-1');
+const buttonTwo = document.querySelector('#link-img-2');
+
+buttonOne.addEventListener("click", (e) => {
+  buttonTwo.click();
+});
+
+buttonTwo.addEventListener("click", (e) => {
+  buttonTwo.style.backgroundColor = "green";
+});
