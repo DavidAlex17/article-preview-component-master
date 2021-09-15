7b2dcbdb7d0256d3be21a0108945d3ecfe97356b
David Alex <DavidAlexx17@icloud.com>
2021-09-15 08:37:44 -0500

    js changes

--- js/script.js
+++ js/script.js
@@ -1,28 +1,13 @@
-var normal = document.querySelector('#normal-state');
-var active = document.querySelector('#active-state');
-var triangle = document.querySelector('#triangle');
-var arrow = document.querySelector('#link-image-1')
-var arrow2 = document.querySelector('#link-image-2');
+let normal = document.querySelector("#normal-state");
+let active = document.querySelector("#active-state");
+let triangle = document.querySelector("#triangle");
+let buttonOne = document.querySelector("#button-1")
+let buttonTwo = document.querySelector("#button-2");
+let clickStatus = false;
 
-// hides the author section, shows the footer - keeps the author section visible in desktop view when share arrow is clicked
-function showActive() {
-    active.style.display = "block";
-    active.style.position = "auto";
-    normal.style.display = "none";
-    triangle.style.display = "block";
-    if($(window).width() > 375){
-        normal.style.display = "flex";
-        arrow2.style.display = "block";
-        arrow.style.display = "none";
-    }
-}
+active.style.display = "none";
 
-// hides the footer and triangle, shows the author section when share arrow2 is clicked
-function hideActive() {
-    active.style.display = "none";
-    triangle.style.display = "block";
-    normal.style.display = "flex";
-    if(triangle.style.display = "block"){
-        arrow.style.display = "flex";
-    }
-}
+normal.addEventListener("click", toggle => {
+  normal.style.display = "none";
+  active.style.display = "inline-flex";
+})
